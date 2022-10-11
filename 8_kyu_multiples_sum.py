6006c800af46427c3aad50c9df84f481e012d727
Ivan <vanesiotanaev@mail.ru>
2022-10-11 19:33:43 +0300

    task 1 modified

--- 8_kyu_multiples_sum.py
+++ 8_kyu_multiples_sum.py
@@ -17,3 +17,13 @@ def solution(number):
         return sum
 
 print(solution(4))
+
+# Второй вариант
+
+def solution(number):
+    if number < 0:
+        return 0
+    else:
+        return sum(x for x in range(number) if x % 3 == 0 or x % 5 == 0)
+
+print(solution(4))
